d8006a1f17152c930e9c54e95650fb5850704ce2
Anton Matosov <anton.matosov@gmail.com>
2017-07-08 23:53:42 -0700

    Reformat file

--- conanfile.py
+++ conanfile.py
@@ -8,20 +8,21 @@ class ArduinoConan(ConanFile):
     license = "Mozilla Public License, v. 2.0 http://mozilla.org/MPL/2.0/"
     url = "https://github.com/Dr-QP/conan-arduino"
 
-    settings = {"os": ["Arduino"],
-                "compiler": {
-                    "gcc": {
-                        "version": ["4.3", "4.5", "4.8", "4.9"],
-                        "libcxx": ["libstdc++", "libstdc++11"]
-                    }
-                },
-                "arch": ["armv6", "armv7", "armv7hf", "avr"]
-                }
+    settings = {
+        "os": ["Arduino"],
+        "compiler": {
+            "gcc": {
+                "version": ["4.3", "4.5", "4.8", "4.9"],
+                "libcxx": ["libstdc++", "libstdc++11"]
+            }
+        },
+        "arch": ["armv6", "armv7", "armv7hf", "avr"]
+    }
     exports_sources = "cmake/*", "!build/*", "!test_package/*", "!**/.DS_Store"
 
     def package(self):
         self.copy("cmake/*", dst="", src=".")
 
     def package_info(self):
-        self.env_info.CMAKE_TOOLCHAIN_FILE = os.path.join(self.package_folder, "cmake", "ArduinoToolchain.cmake")
-
+        self.env_info.CMAKE_TOOLCHAIN_FILE = os.path.join(
+            self.package_folder, "cmake", "ArduinoToolchain.cmake")
