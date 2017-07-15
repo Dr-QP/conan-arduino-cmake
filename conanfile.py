23a5d30aba0bb1537b61426c0483cadb7bbcb634
Anton Matosov <anton.matosov@gmail.com>
2017-07-15 13:29:13 -0700

    Add support for arduino installation on linux

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,5 @@
 from conans import ConanFile
+from conans.tools import os_info, SystemPackageTool
 import os
 
 
@@ -10,6 +11,13 @@ class ArduinoConan(ConanFile):
     description = "Arduino build toolchain. Use it with build_requires"
     settings = "os", "compiler", "arch"
     exports_sources = "cmake/*", "!build/*", "!test_package/*", "!**/.DS_Store"
+    options = {
+        "arduino_version": ["1.8.3", "ANY", "none"],
+        "arduino_path": "ANY"
+    }
+    default_options = "arduino_version=none", "arduino_path=none"
+
+    arduino_path = ""
 
     def configure(self):
         archs = ("armv6", "armv7", "armv7hf", "avr")
@@ -31,11 +39,30 @@ class ArduinoConan(ConanFile):
         self.info.settings.compiler = ""
         self.info.settings.compiler.version = ""
 
+    def build(self):
+        self.arduino_path = str(self.options.arduino_path)
+        if self.options.arduino_version != "none" and self.arduino_path == "none":
+            if os_info.is_linux:
+                installer = SystemPackageTool()
+                installer.install("openjdk-8-jre")
+                installer.install("openjdk-8-jre-headless")
+                installer.install("curl")
+                installer.install("xz-utils")
+
+                self.run("curl https://downloads.arduino.cc/arduino-%s-linux64.tar.xz -o /tmp/arduino.tar.xz" %
+                        self.options.arduino_version)
+                self.run("tar xvfJ /tmp/arduino.tar.xz")
+
+                self.arduino_path = "/tmp/arduino-%s/" % self.options.arduino_version
+                self.run("%s/install.sh" % self.arduino_path)
+
+
     def package(self):
         self.copy("cmake/*", dst="", src=".")
 
     def package_info(self):
         self.env_info.CMAKE_HOST_SYSTEM_NAME = "Arduino"
+        self.env_info.ARDUINO_SDK_PATH = self.arduino_path
         self.env_info.CONAN_CMAKE_TOOLCHAIN_FILE = os.path.join(
             self.package_folder, "cmake", "ArduinoToolchain.cmake")
 
