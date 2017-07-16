7dbebb0100638cb6b020662acb5980a5ff54b426
Anton Matosov <anton.matosov@gmail.com>
2017-07-16 11:11:14 -0700

    Add builds

--- build.py
+++ build.py
@@ -1,7 +1,26 @@
 from conan.packager import ConanMultiPackager
-
+from conans.tools import os_info
+import copy
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(username="anton-matosov", channel="stable")
-    
+    builder = ConanMultiPackager(args="--build missing")
+    builder.add(options={
+        "conan-ardiono-sdk:use_bundled_java": False
+    })
+    if os_info.is_linux or os_info.is_windows:
+        builder.add(options={
+            "conan-ardiono-sdk:use_bundled_java": True
+        })
+
+    if os_info.is_linux:
+        filtered_builds = []
+        for settings, options, env_vars, build_requires in builder.builds:
+            filtered_builds.append(
+                [settings, options, env_vars, build_requires])
+            new_options = copy.copy(options)
+            new_options["conan-ardiono-sdk:host_os"] = "linux32"
+            filtered_builds.append(
+                [settings, new_options, env_vars, build_requires])
+        builder.builds = filtered_builds
+
     builder.run()
