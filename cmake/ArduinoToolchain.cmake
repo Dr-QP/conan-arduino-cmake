7998584393bd1fddfb0ed8fe7a2651d86a28ac60
Anton Matosov <anton.matosov@gmail.com>
2017-07-30 22:49:54 -0700

    Implement new lookup scheme to work with direct path to app and not look for system versions

--- cmake/ArduinoToolchain.cmake
+++ cmake/ArduinoToolchain.cmake
@@ -16,54 +16,25 @@ if(EXISTS  ${CMAKE_CURRENT_LIST_DIR}/Platform/Arduino.cmake)
     set(CMAKE_MODULE_PATH  ${CMAKE_MODULE_PATH} ${CMAKE_CURRENT_LIST_DIR})
 endif()
 
+set(CONAN_ARDUINO_SDK_PATH $ENV{CONAN_ARDUINO_SDK_PATH})
+
 #=============================================================================#
 #                         System Paths                                        #
 #=============================================================================#
 if(UNIX)
     include(Platform/UnixPaths)
     if(APPLE)
-        list(APPEND CMAKE_SYSTEM_PREFIX_PATH $ENV{CONAN_ARDUINO_SDK_PATH})
+        list(APPEND CMAKE_SYSTEM_PREFIX_PATH $CONAN_ARDUINO_SDK_PATH)
     endif()
 elseif(WIN32)
     include(Platform/WindowsPaths)
 endif()
 
-
-#=============================================================================#
-#                         Detect Arduino SDK                                  #
-#=============================================================================#
-if(NOT ARDUINO_SDK_PATH)
-    set(ARDUINO_PATHS)
-
-    foreach(DETECT_VERSION_MAJOR 1)
-        foreach(DETECT_VERSION_MINOR RANGE 5 0)
-            list(APPEND ARDUINO_PATHS arduino-${DETECT_VERSION_MAJOR}.${DETECT_VERSION_MINOR})
-            foreach(DETECT_VERSION_PATCH  RANGE 3 0)
-                list(APPEND ARDUINO_PATHS arduino-${DETECT_VERSION_MAJOR}.${DETECT_VERSION_MINOR}.${DETECT_VERSION_PATCH})
-            endforeach()
-        endforeach()
-    endforeach()
-
-    foreach(VERSION RANGE 23 19)
-        list(APPEND ARDUINO_PATHS arduino-00${VERSION})
-    endforeach()
-
-    if(UNIX)
-        file(GLOB SDK_PATH_HINTS $ENV{CONAN_ARDUINO_SDK_PATH}/arduino*)
-    elseif(WIN32)
-        set(SDK_PATH_HINTS "$ENV{CONAN_ARDUINO_SDK_PATH}\\Arduino")
-    endif()
-    list(SORT SDK_PATH_HINTS)
-    list(REVERSE SDK_PATH_HINTS)
-endif()
-
 find_path(ARDUINO_SDK_PATH
           NAMES lib/version.txt
           PATH_SUFFIXES share/arduino
-                        Arduino.app/Contents/Resources/Java/
-                        Arduino.app/Contents/Java/
-                        ${ARDUINO_PATHS}
-          HINTS ${SDK_PATH_HINTS}
+                        Contents/Java
+          HINTS ${CONAN_ARDUINO_SDK_PATH}
           DOC "Arduino SDK path.")
 
 if(ARDUINO_SDK_PATH)
