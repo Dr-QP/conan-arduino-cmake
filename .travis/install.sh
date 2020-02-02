7462f72740c24d4d4f9afddb999ddda0f754a1aa
Anton Matosov <anton.matosov@gmail.com>
2020-02-01 22:53:14 -0800

    use gcc version 7 to avoid compiler conflict and use latest osx image and don't touch brew/python3

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,8 +4,8 @@ set -e
 set -x
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew update
-    brew upgrade python
+    # brew update
+    # brew upgrade python
 fi
 
 pip3 install conan conan_package_tools --upgrade
